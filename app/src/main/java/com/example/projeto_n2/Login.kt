f7c02d476203b92062d62ae74dd9a499a1a3d66e
Carlos Eduardo A. Ximenes <78832858+CarlosEAXimenes@users.noreply.github.com>
2024-05-20 21:44:09 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projeto_n2/Login.kt
@@ -0,0 +1,10 @@
+package com.example.projeto_n2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
